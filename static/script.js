17995a73f478a9d4cd8f8fde5bed2840a5970455
Thelordmy <m.khairoune@mundiapolis.ma>
2026-03-09 21:18:45 +0000

    added mower manual controll

--- static/script.js
+++ static/script.js
@@ -1,1 +1,9 @@
-
+function send(cmd) {
+    fetch('/command', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: 'cmd=' + cmd
+    })
+    .then(r => r.text())
+    .then(r => document.getElementById('status').innerText = 'Status: ' + r);
+}
